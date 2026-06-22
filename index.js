0ec0b417c6a133fcef825aacc75de05249fbf3e1
Md Limon <fhlimon360@gmail.com>
2026-06-22 20:25:18 +0600

    added subscription

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://digital_life:deZfwRBUBu9yGuU6@cluster0.3036qk8.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
